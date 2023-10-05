bc86b1e3cc5343cef11fb22bc0e501838b74e8f8
RAFhub6 <72543405+RAFhub6@users.noreply.github.com>
2023-10-05 04:08:06 +0000

    e

--- images.js
+++ images.js
@@ -4,7 +4,7 @@ if (args.length < 2) {
   console.error('Please enter at least command');
   process.exit(1); //an error occurred
 }
-if (args[0] == "create"){}
+if (args[0] == "create"){console.log("wip")}
 else {
   console.log("wip")
 }
